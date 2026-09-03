f00946129fe4278f7a60f6e34d07d7032e64394d
EPICGameGuy <admin@cactus.vg>
2026-09-03 19:52:41 -0400

    Changed wasm-opt to optimize for size

--- compile.sh
+++ compile.sh
@@ -7,4 +7,4 @@ echo "Compiling wasm-bindgen..."
 wasm-bindgen --out-name esp-viewer --out-dir wasm/target --target web target/wasm32-unknown-unknown/release/esp-viewer.wasm
 
 echo "Optimizing WebAssembly..."
-wasm-opt wasm/target/esp-viewer_bg.wasm -O3 -o wasm/target/esp-viewer_bg.wasm
+wasm-opt wasm/target/esp-viewer_bg.wasm -Oz --strip-debug -o wasm/target/esp-viewer_bg.wasm
