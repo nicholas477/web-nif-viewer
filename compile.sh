8bbd2f30777566b66a459015268af4f11219d004
EPICGameGuy <nicholas2.chalkley@gmail.com>
2026-09-06 15:30:13 -0400

    Adventures in reducing binary size

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ echo "Compiling wasm-bindgen..."
 wasm-bindgen --out-name esp-viewer --out-dir wasm/target --target web target/wasm32-unknown-unknown/release/esp-viewer.wasm
 
 echo "Optimizing WebAssembly..."
-wasm-opt wasm/target/esp-viewer_bg.wasm -Oz --strip-debug -o wasm/target/esp-viewer_bg.wasm
+wasm-opt wasm/target/esp-viewer_bg.wasm -Oz --enable-bulk-memory --strip-debug -o wasm/target/esp-viewer_bg.wasm
 
 echo "Copying assets..."
 rm -rf wasm/assets
