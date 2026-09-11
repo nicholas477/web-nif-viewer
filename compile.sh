7170e27eabb1d9ba55f54d1e6693d24e80422b0a
EPICGameGuy <nicholas2.chalkley@gmail.com>
2026-09-11 02:06:13 -0400

    Deploy deploy

--- compile.sh
+++ compile.sh
@@ -9,6 +9,8 @@ echo "Compiling wasm-bindgen..."
 wasm-bindgen --out-name esp-viewer --out-dir wasm/target --target web target/wasm32-unknown-unknown/release/esp-viewer.wasm
 
 echo "Optimizing WebAssembly..."
+
+whereis wasm-opt
 wasm-opt --version
 wasm-opt wasm/target/esp-viewer_bg.wasm -Oz --enable-bulk-memory --enable-nontrapping-float-to-int --strip-debug -o wasm/target/esp-viewer_bg.wasm
 
